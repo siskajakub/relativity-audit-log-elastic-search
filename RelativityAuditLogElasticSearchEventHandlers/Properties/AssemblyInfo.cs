387e4e246b624961b891ec11ce3d934c9118a344
Jakub Siska <46302531+siskajakub@users.noreply.github.com>
2021-03-08 00:18:08 +0100

    Copyright adjustment

--- RelativityAuditLogElasticSearchEventHandlers/Properties/AssemblyInfo.cs
+++ RelativityAuditLogElasticSearchEventHandlers/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Jakub Siska")]
 [assembly: AssemblyProduct("Relativity Audit Log Elastic Search EventHandlers")]
-[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyCopyright("Jakub Siska © 2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
